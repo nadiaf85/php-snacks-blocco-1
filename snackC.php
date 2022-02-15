39c0ebf6762e0e50baf6392685f27248d6037677
nadiaf85 <nadia.fiorio@alice.it>
2022-02-15 16:54:19 +0100

    snackC finito

--- snackC.php
+++ snackC.php
@@ -4,4 +4,29 @@ non dovrà contenere lo stesso numero più di una volta -->
 
 <?php
 
+$arrayNumbers = [];
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SnackC</title>
+</head>
+<body>
+    
+    <?php
+        while(count($arrayNumbers) < 15){
+            $numbers = rand (1,100);
+            if(!in_array($numbers, $arrayNumbers)){
+                $arrayNumbers[] = $numbers;
+            }
+        }
+
+        var_dump($arrayNumbers);
+    ?>
+</body>
+</html>
