8de15e8522bcce9943eac9761fd762d1950f08dc
nadiaf85 <nadia.fiorio@alice.it>
2022-02-15 14:43:25 +0100

    creata repo

---
+++ snackC.php
@@ -0,0 +1,3 @@
+<!-- Snack C
+Creare un array con 15 numeri casuali, tenendo conto che l’array
+non dovrà contenere lo stesso numero più di una volta -->
---
+++ snackD.php
@@ -0,0 +1,3 @@
+<!-- Snack D
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere
+il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
